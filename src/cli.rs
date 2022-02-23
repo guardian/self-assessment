db7b82fc608ad45ed807fe5adb1139b2812d9530
Mario Savarese <mario.savarese@theguardian.com>
2022-02-23 23:05:59 +0000

    Update CLI comments

--- src/cli.rs
+++ src/cli.rs
@@ -4,11 +4,11 @@ use serde::Serialize;
 #[derive(Parser, Debug, Serialize)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// Match PRs that were created after this date
+    /// Match PRs and Trello cards that were created after this date
     #[clap(short, long, default_value = "*")]
     pub from: String,
 
-    /// Match PRs that were created up until this date
+    /// Match PRs and Trello cards that were created up until this date
     #[clap(short, long, default_value = "*")]
     pub to: String,
 
@@ -23,11 +23,16 @@ pub struct Args {
     /// Trello API key.
     /// You can get an API key at https://trello.com/app-key
     /// Note: you need to be logged into Trello to be able to see the page.
+    /// Both the API key and the server token need to be set for the generated report
+    /// to include Trello cards.
     #[clap(long)]
     #[serde(rename = "trello-key")]
     pub trello_key: Option<String>,
 
     /// Trello server token.
+    /// You can get a server token by following the link at https://trello.com/app-key
+    /// Both the API key and the server token need to be set for the generated report
+    /// to include Trello cards.
     #[clap(long)]
     #[serde(rename = "trello-token")]
     pub trello_token: Option<String>,
